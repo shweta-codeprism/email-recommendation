7055d62e2e3f0679b3cc6d8c600fa603f78d1e9c
shweta-codeprism <kumar.mimi@gmail.com>
2023-07-13 18:56:06 +0530

    optimize image

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
+const sharp = require("sharp");
 
 const app = express();
 const port = process.env.PORT || 3030;
@@ -59,13 +60,30 @@ app.get("/image", async (req, res) => {
 
     // Take a screenshot of the page
     const screenshot = await page.screenshot({ type: "png" });
+    const outputImagePath = "compressed_image.jpg";
+    // Compression options
+    const compressionOptions = {
+      quality: 80, // Adjust the quality value (0-100) to change compression level
+      chromaSubsampling: "4:4:4" // Adjust chroma subsampling for JPEG format (optional)
+    };
+
+    // Compress the image
+    sharp(screenshot)
+      .jpeg(compressionOptions)
+      .toFile(outputImagePath)
+      .then(() => {
+        console.log("Image compressed successfully!");
+      })
+      .catch(error => {
+        console.error("Error compressing image:", error);
+      });
 
     // Set the response headers
     res.setHeader("Content-Type", "image/png");
-    res.setHeader("Content-Disposition", "attachment; filename=screenshot.png");
+    res.setHeader("Content-Disposition", "attachment; filename=screenshot.jpg");
 
     // Send the image data
-    res.send(screenshot);
+    res.send(outputImagePath);
 
     // Close the browser
     await browser.close();
